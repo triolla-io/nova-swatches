e3e88e17785c54ebab1003d3b29e06f99ec68ee6
zippoxer <zippoxer@gmail.com>
2020-04-06 17:27:47 +0300

    import

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,6 @@
 import Clipboard from 'v-clipboard'
 import "vue-swatches/dist/vue-swatches.min.css";
+import Swatches from "vue-swatches";
 
 Nova.booting((Vue, router, store) => {
   Vue.use(Clipboard)
@@ -7,5 +8,5 @@ Nova.booting((Vue, router, store) => {
   Vue.component('index-nova-swatches', require('./components/IndexField'))
   Vue.component('detail-nova-swatches', require('./components/DetailField'))
   Vue.component('form-nova-swatches', require('./components/FormField'))
-  Vue.component('swatches', require('vue-swatches'))
+  Vue.component('swatches', Swatches)
 })
